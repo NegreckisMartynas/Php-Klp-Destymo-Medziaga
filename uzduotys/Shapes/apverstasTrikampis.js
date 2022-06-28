c01eb80a955e9b2fecba3594c15085a32bf99bfc
Martynas <martynas.negreckis@bit.lt>
2022-06-28 14:22:46 +0300

    Homework tasks

---
+++ uzduotys/Shapes/apverstasTrikampis.js
@@ -0,0 +1,10 @@
+
+
+apvestrasTrikampis(5)
+
+// *****
+// ****
+// ***
+// **
+// *
+// Du variantai: for nuo num iki 0, for nuo 0 iki num;
---
+++ uzduotys/Shapes/daugyboslentele.js
@@ -0,0 +1,28 @@
+function daugyba(x, y) {
+    return x + ' x ' + y + ' = ' + (x*y) 
+}
+
+
+console.log(daugyba(5, 7))
+
+daugybosLentele(5) 
+// 5 x 1 = 5
+// 5 x 2 = 10
+// 5 x 3 = 15
+// 5 x 4 = 20
+// 5 x 5 = 25
+// 5 x 6 = 30
+// 5 x 7 = 35
+// 5 x 8 = 40
+// 5 x 9 = 45
+
+daugybosLenteleVisa()
+// daugymos lentele 1 * _
+// daugymos lentele 2 * _
+// daugymos lentele 3 * _
+// daugymos lentele 4 * _
+// daugymos lentele 5 * _
+// daugymos lentele 6 * _
+// daugymos lentele 7 * _
+// daugymos lentele 8 * _
+// daugymos lentele 9 * _
