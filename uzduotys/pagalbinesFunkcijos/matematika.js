30e1c45c9557746d8dbbf95fa08788b2373297f7
Martynas <martynas.negreckis@bit.lt>
2022-06-29 14:16:38 +0300

    matematika

---
+++ uzduotys/pagalbinesFunkcijos/matematika.js
@@ -0,0 +1,21 @@
+16 + 5;
+16 - 6;
+16 * 5;
+16 / 5;
+19 % 15;
+10 ** 3;
+
+
+console.log('19 / 5 =', Math.floor(19 / 5))
+console.log('19 % 5 =', 19%15)
+console.log('10 ** 3 =', 10**3)
+console.log('sqrt(100) =', Math.sqrt(100))
+console.log('1000 ** (1/3) =', Math.round((1000 ** (1/3)) * 10000) /10000)
+
+let price = 50.00;
+console.log('20% of 50.00 = ', price/100 * 20) 
+console.log('40% of 50.00 = ', price * 0.4) 
+
+
+console.log(189 % 9 === 0); // jei salyga teisinga, 189 yra dalus is 9
+console.log(88 % 2 === 0); // jei teisinga, 88 yra lyginis
