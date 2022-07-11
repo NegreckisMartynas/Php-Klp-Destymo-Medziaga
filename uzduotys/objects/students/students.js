653f478f9116b85aa33e39881272ab1e2362bc67
Martynas Negreckis <negreckis.martynas@gmail.com>
2022-07-11 10:40:30 +0300

    objects/students

---
+++ uzduotys/objects/students/students.js
@@ -0,0 +1,30 @@
+const data = require('./data.js')
+
+// Parašyti funkcijas: 
+
+// Atspausdinti studentų vardus ir pavardes
+
+
+
+// Suskaičiuoti studento pažymių vidurkį
+
+
+
+// Atspausdinti studento vardą, pavardę bei jo pažymių vidurkį
+
+
+
+// Išrinkti tik pažymius, kurie mažensi už 4
+
+
+
+// Atspausdinti studentus, kurių bent vienas pažimys mažesnis už 4
+
+
+
+// Surikiuoti studento pažymius didėjimo tvarka
+data[0].forEach(e => e.fun = () => console.log(e));
+data[0][0].fun(data[0][0]);
+
+
+// Surikiuoti studentus pagal jų vidurkius didėjimo tvarka
