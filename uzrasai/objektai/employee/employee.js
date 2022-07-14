9a5b805246b6d6ea48e29db1f7f4ef774fd9ede6
Martynas <martynas.negreckis@bit.lt>
2022-07-14 14:02:58 +0300

    Secret employee

--- uzrasai/objektai/employee/employee.js
+++ uzrasai/objektai/employee/employee.js
@@ -8,6 +8,11 @@ function employee(firstName, lastName, salary) {
         },
         raiseSalary(raise) {
             this.salary += raise;
+        },
+        toString() {
+            return 'Employee "' + this.vardas + 
+                   ' ' + this.lastName +
+                   '"; Salary: ' + this.salary
         }
     }
 }
@@ -19,7 +24,27 @@ function volunteer(firstName, lastName) {
         calculateWages(hours) {
             return 0;
         },
-        raiseSalary(raise) {}
+        raiseSalary(raise) {},
+        toString() {
+            return 'Volunteer "' + this.vardas + ' ' + this.lastName + '"'
+        }
+    }
+}
+function boss(firstName, lastName) {
+    return {
+        vardas: firstName,
+        lastName,
+        salary: 100,
+        calculateWages(hours) {
+            return this.salary * hours;
+        },
+        raiseSalary(raise) {
+            this.salary += raise * 2;
+        },
+        toString() {
+            return 'CEO "' + this.vardas + ' ' + this.lastName + 
+                   '"; Salary: ' + this.salary;
+        }
     }
 }
 
@@ -37,21 +62,33 @@ console.log('after raise:', emp2.calculateWages(20));
 console.log(emp1);
 
 const employees = [
+    boss('Boss', 'Ceo'),
     employee('Vard', 'Pav', 20),
     employee('Foo', 'Bar', 25.2),
     volunteer('Volun', 'Teer'), //unpaid, salary never increases
-    employee('Int', 'Ern', 0), //unpaid for now, but paid after salary increase
+    employee('Int', 'Ern', 0),
+    {
+        calculateWages() {return 10},
+        raiseSalary(raise) { console.log('Salaries raised:', raise )},
+        toString() { return 'Secret employee'}
+    }, //unpaid for now, but paid after salary increase
 ]
 
 function wagesPaid(workHours, employees) {
     return employees.map(emp => emp.calculateWages(workHours))
                     .reduce( (sum, n) => sum + n);
 }
+function printEmployees(employees) {
+    employees.map( emp => emp.toString())
+             .forEach( str => console.log(str));
+}
+
 console.log('Wages paid for employees after 1 hour:',
             wagesPaid(1, employees));
 employees.forEach(emp => emp.raiseSalary(10));
 console.log('Wages paid for employees after 1 hour:',
             wagesPaid(1, employees));
+printEmployees(employees);
 
 //emp1 = {1}
 // --emp2 = emp1 = {1}
