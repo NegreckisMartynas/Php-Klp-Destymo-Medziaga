9a2c82104f69704596b3be0f970293ad911f704c
Martynas <martynas.negreckis@bit.lt>
2022-07-14 13:33:25 +0300

    Employee and volunteer

--- uzrasai/objektai/employee/employee.js
+++ uzrasai/objektai/employee/employee.js
@@ -5,9 +5,24 @@ function employee(firstName, lastName, salary) {
         salary,
         calculateWages(hours) {
             return this.salary * hours;
+        },
+        raiseSalary(raise) {
+            this.salary += raise;
         }
     }
 }
+function volunteer(firstName, lastName) {
+    return {
+        vardas: firstName,
+        lastName,
+        salary: 0,
+        calculateWages(hours) {
+            return 0;
+        },
+        raiseSalary(raise) {}
+    }
+}
+
 
 let emp1 = employee('Vardenis', 'Pavardenis', 20);
 let emp2 = emp1;
@@ -15,16 +30,30 @@ emp1 = employee('Vard', 'Pav', 22.2);
 
 console.log(emp1.calculateWages(20));
 console.log(emp1.lastName);
-console.log(emp2.calculateWages(20));
+console.log('before raise:', emp2.calculateWages(20));
 console.log(emp2.lastName);
+emp2.raiseSalary(0.5);
+console.log('after raise:', emp2.calculateWages(20));
 console.log(emp1);
 
+const employees = [
+    employee('Vard', 'Pav', 20),
+    employee('Foo', 'Bar', 25.2),
+    volunteer('Volun', 'Teer'), //unpaid, salary never increases
+    employee('Int', 'Ern', 0), //unpaid for now, but paid after salary increase
+]
+
+function wagesPaid(workHours, employees) {
+    return employees.map(emp => emp.calculateWages(workHours))
+                    .reduce( (sum, n) => sum + n);
+}
+console.log('Wages paid for employees after 1 hour:',
+            wagesPaid(1, employees));
+employees.forEach(emp => emp.raiseSalary(10));
+console.log('Wages paid for employees after 1 hour:',
+            wagesPaid(1, employees));
 
 //emp1 = {1}
 // --emp2 = emp1 = {1}
 //emp1 = {2}
 // emp1 = {2}, --emp2 = {1};
-const arr = ['a','b'];
-const temp = arr[0]; //temp = 'a'; arr = ['a', 'b'];
-arr[0] = arr[1] // temp = 'a'; arr = ['b', 'b']
-arr[1] = temp; // temp = 'a'; arr = ['b', 'a']
