7815d29a95c44a16f0514f574fbf12fd6f35db46
Martynas <martynas.negreckis@bit.lt>
2022-07-25 15:00:40 +0300

    Forms advanced example and calc hints

--- uzrasai/form/script.js
+++ uzrasai/form/script.js
@@ -1,5 +1,5 @@
 function sendRegistration(event) {
-  if(!validateRegistration(event.target)){
+  if(!validateRegistrationFunctions(event.target)){
     event.preventDefault();
   }
   else {
@@ -9,6 +9,43 @@ function sendRegistration(event) {
 }
 
 function validateRegistration(form) {
+  let result = true;
+  if(form.elements.username.value.length < 5) {
+    form.elements.username.classList.add('error')
+    form.querySelector('#username-error').classList.remove('hidden')
+    form.querySelector('#username-error').innerText
+      = 'Username too short!';
+    result = false;
+  }
+  const password = form.elements.password;
+  const confirm = form.elements.confirm;
+  if(password.value !== confirm.value) {
+    form.elements.password.classList.add('error');
+    form.elements.confirm.classList.add('error');
+    form.querySelector('#password-error').classList.remove('hidden');
+    form.querySelector('#password-error').innerText
+        = 'Passwords don\'t match!';
+    result = false;
+  }
+  result = emailIsValid(form.elements.email) && result;
+  return result;
+}
+
+function emailIsValid(email) {
+  //...
+  return true; // jei teisingas
+  // return false jei netiesingas
+}
+
+function validateRegistrationFunctions(form) {
+  let result = true;
+  result = validateUsername(form) && result;
+  result = validatePassword(form) && result;
+  result = emailIsValid(form.elements.email) && result;
+  return result;
+}
+
+function validateUsername(form) {
   if(form.elements.username.value.length < 5) {
     form.elements.username.classList.add('error')
     form.querySelector('#username-error').classList.remove('hidden')
@@ -16,9 +53,12 @@ function validateRegistration(form) {
       = 'Username too short!';
     return false;
   }
+  return true;
+}
+
+function validatePassword(form) {
   const password = form.elements.password;
   const confirm = form.elements.confirm;
-  console.log(password.value, confirm.value);
   if(password.value !== confirm.value) {
     form.elements.password.classList.add('error');
     form.elements.confirm.classList.add('error');
@@ -27,6 +67,5 @@ function validateRegistration(form) {
         = 'Passwords don\'t match!';
     return false;
   }
-
   return true;
 }
